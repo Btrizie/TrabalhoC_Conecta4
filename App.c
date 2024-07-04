6afd957bb9a8e7f509cf919491fc85845e02b651
BiaCavalari <141246725+Btrizie@users.noreply.github.com>
2024-07-04 08:52:37 -0300

    Update App.c

--- App.c
+++ App.c
@@ -6,6 +6,8 @@
 
 int verificaColuna (int x);
 int verificaLinha (int coluna, int matriz[6][7]);
+void mostrarTabuleiro(int matriz[6][7]);
+bool haVencedores(int matriz[6][7], int coluna, int linha);
 
 int main(int argc, char** argv){
   SDL_Init(SDL_INIT_EVERYTHING);
@@ -16,10 +18,11 @@ int main(int argc, char** argv){
   SDL_Texture * circleR = IMG_LoadTexture(renderer, "CRed.png"); 
   SDL_Texture * circleY = IMG_LoadTexture(renderer, "CYellow.png"); 
 
-//1, quando for a vez do jogador 1, e 2, quando for a vez do jogador 2  
+  //1, quando for a vez do jogador 1, e 2, quando for a vez do jogador 2  
   int jogador = 1;
+  bool venceu = false;
 
-//matriz que representa o tabuleiro
+  //matriz que representa o tabuleiro
   int matriz[6][7];
     for (int i = 0; i < 6; ++i) {
         for (int j = 0; j < 7; ++j) {
@@ -27,31 +30,16 @@ int main(int argc, char** argv){
         }
     }
 
-//interface tabuleiro
+  //interface tabuleiro
   SDL_Rect quad1, quad2, quad3; 
-//quad1 --> é relacionado ao tabuleiro
+  //quad1 --> é relacionado ao tabuleiro
   quad1.x = 20;
   quad1.y = 20;
   quad1.w = 900;
   quad1.h = 610;
 
-//quad2 --> é relacionado a pecinha Vermelha
-  //--> posição da imagem
-  quad2.x = 27;
-  quad2.y = 25;
-  //--> achatar imagem
-  quad2.w = 110;
-  quad2.h = 85;
 
-//quad3 --> é relacionado a pecinha Amarela
-  //--> posição da imagem
-  quad3.x = 157;
-  quad3.y = 25;
-  //--> achatar imagem
-  quad3.w = 110;
-  quad3.h = 85;
-
-//evento --> jogo
+  //evento --> jogo
   while(true)
   {
     SDL_Event event;
@@ -61,7 +49,7 @@ int main(int argc, char** argv){
     
     while(SDL_PollEvent(&event)){
       //sair do jogo
-      if( event.type == SDL_QUIT ){
+      if( event.type == SDL_QUIT || venceu){
         exit(0);
       }
 
@@ -72,65 +60,50 @@ int main(int argc, char** argv){
         
         int x, y;
         x = verificaColuna (mouse_x);
-        y = verificaLinha (mouse_y, matriz);
+        printf("%d",x);
+        y = verificaLinha (x, matriz);
+        printf("%d",y);
 
-        if (x >= 0 && y >= 0)
-        {
-          matriz[x][y] = jogador; 
-        }
+        printf("\nColuna (0-6) %d escolhida pelo jogador %d", x, jogador);
+
+        matriz[y][x] = jogador; 
+        mostrarTabuleiro(matriz);
         
-        if (jogador == 1)
-          jogador = 2;
-        else 
-          jogador = 1;
-        
-        /*else if( event.type == SDL_MOUSEBUTTONDOWN)
-      {
-            //evento do jogador
-        int mouse_x = event.button.x;
-        int mouse_y = event.button.y;
-        printf("A coord_x eh: %d\n", mouse_x);
-        printf("A coord_y eh: %d\n", mouse_y);
-        if(dentro das coordenadas)
-        {
-          if(com bolinha --> flag)
-          {
-          //erro
-          }
-          else{
-          //coloca a bolinha na posição mouse_x e mouse_y
-          }
+        jogador = (jogador == 1) ? 2 : 1;
+
+        if (haVencedores(matriz, x, y)) {
+            mostrarTabuleiro(matriz);
+            printf("Parabéns! Jogador %d venceu!\n", jogador);
+            venceu = true;
         }
-      //if 4 bolihas mesma cor, win
-      }*/
-        //quad2.x -= 20;
       }
     }
+
     for (int i = 0; i < 6; ++i) 
     {
       for (int j = 0; j < 7; ++j) 
       {
         SDL_Rect posicaoPeca;
-      posicaoPeca.x = 25;
-      posicaoPeca.y = 27;
-      posicaoPeca.w = 110;
-      posicaoPeca.h = 85;
+          //--> posição da imagem
+          posicaoPeca.x = 25;
+          posicaoPeca.y = 27;
+          //--> achatar imagem
+          posicaoPeca.w = 110;
+          posicaoPeca.h = 85;
         if (matriz[i][j] == 1) 
         {
-          posicaoPeca.x += 130 * i;
-          posicaoPeca.y += 102 * j;
+          posicaoPeca.x += 130*j;
+          posicaoPeca.y += 102*i;
           SDL_RenderCopy(renderer, circleR, NULL, &posicaoPeca);
         } 
         else if (matriz[i][j] == 2) 
         {
-          posicaoPeca.x += 130 * i;
-          posicaoPeca.y += 102 * j;
+          posicaoPeca.x += 130*j;
+          posicaoPeca.y += 102*i;
           SDL_RenderCopy(renderer, circleY, NULL, &posicaoPeca);
         }
       }
     } 
-    SDL_RenderCopy(renderer, circleR, NULL, &quad2);   
-    SDL_RenderCopy(renderer, circleY, NULL, &quad3);   
     SDL_RenderPresent(renderer);
   }
 
@@ -144,31 +117,31 @@ int main(int argc, char** argv){
 
 int verificaColuna (int x)
 {
-  if (x > 27 && x < 157) 
+  if (x > 27 && x < 156) 
   {
     return 0;
   }    
-  else if (x < 287)
+  else if (x > 157 && x < 286)
   {
     return 1; 
   }
-  else if (x < 417)
+  else if (x > 287 && x < 416)
   {
     return 2;
   }
-  else if (x < 547)
+  else if (x > 417 && x < 546)
   {
     return 3;
   }
-  else if (x < 677)
+  else if (x > 547 && x < 676)
   {
     return 4;
   }
-  else if (x < 807)
+  else if (x > 677 && x < 806)
   {
     return 5;
   }
-  else if (x < 937) //nao sei o limite do tabuleiro
+  else if (x > 807 && x < 937) //nao sei o limite do tabuleiro
   {
     return 6;
   }
@@ -180,17 +153,94 @@ int verificaColuna (int x)
 
 int verificaLinha (int coluna, int matriz[6][7]) 
 {
-  int linha = 5; 
-  while (linha >= 0)
-  {
-    if (matriz[linha][coluna] > 0)
-    {
-      if (linha == 0)
-        return -1;
-      else
-        return linha - 1;
+    int linha = 5;
+    while (linha >= 0) {
+        if (matriz[linha][coluna] == 0) {
+            return linha;
+        }
+        linha--;
     }
-    linha--;
-  }
-  return 5;
+    return -1; // Retorno caso a coluna esteja cheia (embora o código principal já verifique isso)
+}
+
+//método para checar se o jogo funciona corretamente
+void mostrarTabuleiro(int matriz[6][7]) {
+    printf("\n");
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 7; ++j) {
+            printf("| ");
+            switch (matriz[i][j]) {
+                case 0:
+                    printf(" ");
+                    break;
+                case 1:
+                    printf("V");
+                    break;
+                case 2:
+                    printf("A");
+                    break;
+            }
+            printf(" ");
+        }
+        printf("|\n");
+    }
+    printf("+---------------------------+\n");
+    printf("| 1   2   3   4   5   6   7 |\n\n");
+}
+
+bool haVencedores(int matriz[6][7], int coluna, int linha) {
+    int peca = matriz[linha][coluna];
+    if (peca == 0) {
+        return false;
+    }
+
+    int count = 1;
+    // Verifica na horizontal (direita e esquerda)
+    for (int x = coluna + 1; x < 7 && matriz[linha][x] == peca; ++x) {
+        ++count;
+    }
+    for (int x = coluna - 1; x >= 0 && matriz[linha][x] == peca; --x) {
+        ++count;
+    }
+    if (count >= 4) {
+        return true;
+    }
+
+    count = 1;
+    // Verifica na vertical (baixo e cima)
+    for (int y = linha + 1; y < 6 && matriz[y][coluna] == peca; ++y) {
+        ++count;
+    }
+    for (int y = linha - 1; y >= 0 && matriz[y][coluna] == peca; --y) {
+        ++count;
+    }
+    if (count >= 4) {
+        return true;
+    }
+
+    count = 1;
+    // Verifica na diagonal principal (/)
+    for (int x = coluna + 1, y = linha + 1; x < 7 && y < 6 && matriz[y][x] == peca; ++x, ++y) {
+        ++count;
+    }
+    for (int x = coluna - 1, y = linha - 1; x >= 0 && y >= 0 && matriz[y][x] == peca; --x, --y) {
+        ++count;
+    }
+    if (count >= 4) {
+        return true;
+    }
+
+    count = 1;
+    // Verifica na diagonal secundária (\)
+    for (int x = coluna + 1, y = linha - 1; x < 7 && y >= 0 && matriz[y][x] == peca; ++x, --y) {
+        ++count;
+    }
+    for (int x = coluna - 1, y = linha + 1; x >= 0 && y < 6 && matriz[y][x] == peca; --x, ++y) {
+        ++count;
+    }
+    if (count >= 4) {
+        return true;
+    }
+
+    return false;
 }
